e4b5f64c05047a4e5bb4382cf99750b3ab4d8f4c
mid <mid@KCSK3221.k302.kcsk.ac.jp>
2025-07-14 10:16:41 +0900

    豊田

--- src/DVDrental/DB.java
+++ src/DVDrental/DB.java
@@ -1,5 +1,21 @@
 package DVDrental;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class DB {
+	
+	public static void insertMember(String id, String name) {
+		try(Connection conn = DriverManager.getConnection(URL,USER,PASS);
+				PreparedStatement ps = conn.prepareStatement("INSERT INTO member(id,name)VALUES(?,?)")){
+				ps.setString(1, id);
+				ps.setString(2, name);
+				ps.executeUpdate();
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+	}
 
 }
