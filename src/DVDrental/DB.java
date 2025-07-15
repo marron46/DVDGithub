4b2ba540eef3df4e360b8a06ca616c711acd70a6
mid <mid@KCSK3222.k302.kcsk.ac.jp>
2025-07-15 12:12:37 +0900

    takatori

--- src/DVDrental/DB.java
+++ src/DVDrental/DB.java
@@ -7,22 +7,11 @@ import java.sql.SQLException;
 
 public class DB {
 	
-	public static void insertMember(String id, String name) {
+	public static void insertDVD(String CodeField, String TitleField) {
 		try(Connection conn = DriverManager.getConnection(URL,USER,PASS);
-				PreparedStatement ps = conn.prepareStatement("INSERT INTO member(id,name)VALUES(?,?)")){
-				ps.setString(1, id);
-				ps.setString(2, name);
-				ps.executeUpdate();
-			}catch(SQLException e) {
-				e.printStackTrace();
-			}
-	}
-	
-	public static void insertDVD(String id, String name) {
-		try(Connection conn = DriverManager.getConnection(URL,USER,PASS);
-				PreparedStatement ps = conn.prepareStatement("INSERT INTO member(CodeField,TitleFeild)VALUES(?,?)")){
-				ps.setString(1, CodeField);
-				ps.setString(2, TitleFeild);
+				PreparedStatement ps = conn.prepareStatement("INSERT INTO (CodeField,TitleField)VALUES(?,?)")){
+				ps.setString(1,CodeField);
+				ps.setString(2,TitleField);
 				ps.executeUpdate();
 			}catch(SQLException e) {
 				e.printStackTrace();
