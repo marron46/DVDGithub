a6e3ba22fe8b318007bc58b8ed760e1e32f951ea
mid <mid@KCSK3214.k302.kcsk.ac.jp>
2025-07-25 09:50:23 +0900

    ooba

--- src/DVDrental/BiveBack.java
+++ src/DVDrental/BiveBack.java
@@ -13,10 +13,12 @@ public class BiveBack extends JPanel{
 	public BiveBack(MainFrame Frame){
 	setLayout(new GridLayout(3,2));//レイアウトの作成
 		
-	JButton RentButton = new JButton("返却");
-	JButton TopButton = new JButton("TOP");
-	JTextField CodeField = new JTextField();//ボタンとフィールドの作成
+	//ボタンとフィールドの作成
+	JButton RentButton = new JButton("返却");//返却ボタン
+	JButton TopButton = new JButton("TOP");  //TOPボタン
+	JTextField CodeField = new JTextField(); //DVDコードのテキストフィールド
 	
+	//DB返却処理
 	RentButton.addActionListener(e -> {
 		DB.insertBackDVD(CodeField.getText());
 		JOptionPane.showMessageDialog(this,"返却が完了しました。");//返却処理
@@ -24,10 +26,11 @@ public class BiveBack extends JPanel{
 	
 	TopButton.addActionListener(e -> Frame.showPanel("TOP"));//TOPに戻る
 	
+	//ボタンとフィールドの配置
 	add(new JLabel("DVDコード"));
 	add(CodeField);
 	add(RentButton);
-	add(TopButton);//ボタンとフィールドの配置
+	add(TopButton);
 	
 	}
 	
